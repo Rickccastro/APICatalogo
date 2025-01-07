8e21bac67f856a3cfd3f07b114c6de8fdbb4b849
ricardo <ricardocastroparmezan@gmail.com>
2025-01-06 22:48:28 -0300

    criando Migrations

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -8,5 +8,7 @@ public class Produto
     public decimal Preco { get; set;}
     public string? ImageUrl { get; set;}
     public float Estoque { get; set; }
-    public DateTime DataCadastro { get; set;}   
+    public DateTime DataCadastro { get; set;}
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 }
