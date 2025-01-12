9e5b77cece7744d6191e5acd388dfe92cceb98eb
ricardo <ricardocastroparmezan@gmail.com>
2025-01-11 21:22:51 -0300

    Validacoes Personalizadas 1/2

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -9,15 +10,21 @@ public class Produto
 {
     [Key]
     public int ProdutoId { get; set; }
-    [Required]
-    [StringLength(80)]
+
+    [Required(ErrorMessage ="O nome é obrigatório")]
+    [StringLength(80,ErrorMessage = "O nome deve ter no maximo {1} e no minimo {2}caracteres", MinimumLength =5)]
+    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
+
     [Required]
-    [StringLength(300)]
+    [StringLength(300, ErrorMessage ="A descrição deve ter no máximo {1} caracteres")]
     public string? Descricao { get; set;}
+
+
     [Required]
-    [Column(TypeName ="decimal(10,2)")]
+    [Range(1,1000,ErrorMessage ="0 preço deve estar entre {1} e {2}")]
     public decimal Preco { get; set;}
+
     [Required]
     [StringLength(300)]
     public string? ImageUrl { get; set;}
