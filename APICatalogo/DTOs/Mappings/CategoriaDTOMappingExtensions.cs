99082f52ef04a10b47901426e5cf5f05772d5284
ricardo <ricardocastroparmezan@gmail.com>
2025-01-21 11:24:33 -0300

    Extensions Methods

---
+++ APICatalogo/DTOs/Mappings/CategoriaDTOMappingExtensions.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Models;
+
+namespace APICatalogo.DTOs.Mappings;
+
+public static class CategoriaDTOMappingExtensions
+{
+    public static CategoriaDTO? ToCategoriaDTO(Categoria categoria)
+    {
+        if (categoria == null)
+            return null;
+
+        return new CategoriaDTO
+        {
+            CategoriaId = categoria.CategoriaId,
+            ImageUrl = categoria.ImageUrl,
+            Nome = categoria.Nome,
+        };
+    }
+    public static IEnumerable<CategoriaDTO> ToCategoriaDTOList(IEnumerable<Categoria> categorias)
+    {
+        if (categorias == null || !categorias.Any())
+            return new List<CategoriaDTO>();
+
+      return categorias.Select(categoria => new CategoriaDTO
+      {
+          CategoriaId = categoria.CategoriaId,
+          Nome  =  categoria.Nome,
+          ImageUrl = categoria.ImageUrl 
+      }).ToList();
+    }
+
+    public static Categoria? ToCategoria(this CategoriaDTO categoriaDto)
+    {
+        if (categoriaDto == null)
+            return null;
+
+         return new Categoria
+        {
+            CategoriaId = categoriaDto.CategoriaId,
+            ImageUrl = categoriaDto.ImageUrl,
+            Nome = categoriaDto.Nome,
+        };        
+    }
+}
