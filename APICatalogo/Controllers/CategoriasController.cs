fed27b4ab126b180ccae39e87e294b29019f1e73
ricardo <ricardocastroparmezan@gmail.com>
2025-01-20 21:21:46 -0300

    Mapeando Dto sem Automapper

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using APICatalogo.Models;
+﻿using APICatalogo.DTOs;
+using APICatalogo.Models;
 using APICatalogo.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,45 +15,94 @@ public class CategoriasController : ControllerBase
     }
 
     [HttpGet()]
-    public ActionResult<IEnumerable<Categoria>> GetAll()
+    public ActionResult<IEnumerable<CategoriaDTO>> GetAll()
     {
         var categorias = _unitOfWork.CategoriaRepository.GetAll();
+       
+        var listaCategoriasDto = new List<CategoriaDTO>();
+
+        foreach(var categoria in categorias)
+        {
+            var categoriaDto = new CategoriaDTO()
+            {
+                CategoriaId = categoria.CategoriaId,
+                ImageUrl = categoria.ImageUrl,
+                Nome = categoria.Nome,
+            };
+            listaCategoriasDto.Add(categoriaDto);
+        }
+        
+        
         return Ok(categorias);
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
-    public ActionResult<Categoria> GetById(int id)
+    public ActionResult<CategoriaDTO> GetById(int id)
     {
         var categoriaById = _unitOfWork.CategoriaRepository.Get( c=> c.CategoriaId == id);
         if (categoriaById is null)
             return NotFound("Categoria não encontrado");
-
+        var categoriaDto = new CategoriaDTO()
+        {
+            CategoriaId = categoriaById.CategoriaId,
+            ImageUrl = categoriaById.ImageUrl,
+            Nome = categoriaById.Nome,
+        };
         return Ok(categoriaById);
     }
     [HttpPost]
-    public ActionResult CadastrarCategoria(Categoria categoria)
+    public ActionResult<CategoriaDTO> CadastrarCategoria(CategoriaDTO categoriaDto)
     {
-        if (categoria is null)
+        if (categoriaDto is null)
             return BadRequest();
+
+        var categoria = new Categoria()
+        {
+            CategoriaId = categoriaDto.CategoriaId,
+            ImageUrl= categoriaDto.ImageUrl,
+            Nome= categoriaDto.Nome,      
+        };
+        var novaCategoriaDto = new CategoriaDTO()
+        {
+            CategoriaId = categoria.CategoriaId,
+            ImageUrl = categoria.ImageUrl,
+            Nome = categoria.Nome,
+        };
+
         var categoriaCriada = _unitOfWork.CategoriaRepository.Create(categoria);
         _unitOfWork.Commit();
 
-        return new CreatedAtRouteResult("ObterCategoria", new { id = categoriaCriada.CategoriaId }, categoriaCriada);
+        return new CreatedAtRouteResult("ObterCategoria", new { id = novaCategoriaDto.CategoriaId }, novaCategoriaDto);
     }
 
     [HttpPut("{id:int}")]
-    public ActionResult AtualizarPutCategoria(int id, Categoria categoria)
+    public ActionResult<CategoriaDTO> AtualizarPutCategoria(int id, CategoriaDTO categoriaDto)
     {
-        if (id != categoria.CategoriaId)
+        if (id != categoriaDto.CategoriaId)
             return BadRequest();
-        _unitOfWork.CategoriaRepository.Update(categoria);
+
+        var categoria = new Categoria()
+        {
+            CategoriaId = categoriaDto.CategoriaId,
+            ImageUrl = categoriaDto.ImageUrl,
+            Nome = categoriaDto.Nome,
+        };
+
+        var categoriaAtualizada = _unitOfWork.CategoriaRepository.Update(categoria);
         _unitOfWork.Commit();
 
-        return Ok(categoria);
+        var categoriaAtualizadaDto = new CategoriaDTO()
+        {
+            Nome = categoriaAtualizada.Nome,
+            ImageUrl= categoriaAtualizada.ImageUrl,
+            CategoriaId = categoriaAtualizada.CategoriaId 
+        };
+
+        return Ok(categoriaAtualizadaDto);
     }
 
     [HttpDelete("{id:int}")]
-    public ActionResult DeleteCategoria(int id)
+    public ActionResult<CategoriaDTO> DeleteCategoria(int id)
     {
         var categoria = _unitOfWork.CategoriaRepository.Get(c => c.CategoriaId == id);
         if (categoria is null)
@@ -60,6 +110,14 @@ public class CategoriasController : ControllerBase
 
         var categoriaExcluida = _unitOfWork.CategoriaRepository.Delete(categoria);
         _unitOfWork.Commit();
-        return Ok(categoriaExcluida);
+
+        var categoriaExcluidaDto = new CategoriaDTO()
+        {
+            Nome = categoriaExcluida.Nome,
+            ImageUrl = categoriaExcluida.ImageUrl,
+            CategoriaId = categoriaExcluida.CategoriaId
+        };
+
+        return Ok(categoriaExcluidaDto);
     }
 }
---
+++ APICatalogo/DTOs/CategoriaDTO.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public class CategoriaDTO
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    public string? ImageUrl { get; set; }
+}
+
