a9e812bc53956333fb0cf39e7125174f7171f5f6
ricardo <ricardocastroparmezan@gmail.com>
2025-01-21 13:12:31 -0300

    Implemented AutoMapper

---
+++ APICatalogo/DTOs/ProdutoDTO.cs
@@ -0,0 +1,29 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public class ProdutoDTO
+{
+    [Key]
+    public int ProdutoId { get; set; }
+
+    [Required]
+    [StringLength(80)]
+    [PrimeiraLetraMaiuscula]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? Descricao { get; set; }
+
+
+    [Required]
+    public decimal Preco { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImageUrl { get; set; }
+    public int CategoriaId { get; set; }
+}
