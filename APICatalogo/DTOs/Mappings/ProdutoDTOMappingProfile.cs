fc34078ee0584616b423f7b63eb58ab29f752c37
ricardo <ricardoparmezancastro.dev@gmail.com>
2025-01-26 23:48:01 -0300

    HTTP request methods

--- APICatalogo/DTOs/Mappings/ProdutoDTOMappingProfile.cs
+++ APICatalogo/DTOs/Mappings/ProdutoDTOMappingProfile.cs
@@ -1,4 +1,5 @@
-﻿using APICatalogo.Models;
+﻿using APICatalogo.DTOs.Patch;
+using APICatalogo.Models;
 using AutoMapper;
 using System.Collections.Generic;
 
@@ -9,6 +10,8 @@ public class ProdutoDTOMappingProfile : Profile
 	public ProdutoDTOMappingProfile()
 	{
 			CreateMap<Produto,ProdutoDTO>().ReverseMap();
+			CreateMap<Produto,ProdutoDTOUpdateRequest>().ReverseMap();
+			CreateMap<Produto,ProdutoDTOUpdateResponse>().ReverseMap();
 			CreateMap<Categoria,CategoriaDTO>().ReverseMap();
 	}
 }
--- APICatalogo/DTOs/Patch/ProdutoDTOUpdateResponse.cs
+++ APICatalogo/DTOs/Patch/ProdutoDTOUpdateResponse.cs
@@ -1,28 +1,19 @@
-﻿using APICatalogo.Models;
-using APICatalogo.Validations;
+﻿using APICatalogo.Validations;
 using System.ComponentModel.DataAnnotations;
 
-namespace APICatalogo.DTOs;
+namespace APICatalogo.DTOs.Patch;
 
-public class ProdutoDTO
+public class ProdutoDTOUpdateResponse
 {
-    [Key]
+   
     public int ProdutoId { get; set; }
 
-    [Required]
-    [StringLength(80)]
-    [PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
-    [Required]
-    [StringLength(300)]
     public string? Descricao { get; set; }
 
-    [Required]
     public decimal Preco { get; set; }
 
-    [Required]
-    [StringLength(300)]
     public string? ImageUrl { get; set; }
     public int CategoriaId { get; set; }
 }
